5d55ba48536025857ac1c05f39e7aac7fa14c227
Belard <34962552+Belard@users.noreply.github.com>
2017-12-30 15:39:49 +0000

    Update main.c

--- main.c
+++ main.c
@@ -69,15 +69,218 @@ int insertStudent (student_type student[], int qtd)
     return i;
 }
 
+char form()
+{
+    char r;
+    do
+    {
+        printf("---Main menu---\n");
+        printf("1- Manage students.\n");
+        printf("2- Manage activities.\n");
+        printf("3- Manage teams.\n");
+        printf("4- In progress.\n");
+        printf("5- In progress.\n");
+        printf("s/S- Preencher estruturas.\n");
+        r=getch();
+    }
+    while (r!='s' && r!='S' && r!='1' && r!='2' && r!='3' && r!='4' && r!='5');
+    return r;
+}
+
+char formStudents()
+{
+    char r;
+    do
+    {
+        printf("---Student Section---\n");
+        printf("1- Insert a new student.\n");
+        printf("2- Show students.\n");
+        printf("3- Delete a student.\n");
+        printf("4- Search a student.\n");
+        printf("r/R- Return to menu.\n");
+        r=getch();
+    }
+    while (r!='r' && r!='R' && r!='1' && r!='2' && r!='3' && r!='4');
+    return r;
+}
+
+char formActivity()
+{
+    char r;
+    do
+    {
+        printf("---Activity Section---\n");
+        printf("1- Insert a new activity.\n");
+        printf("2- Show activities.\n");
+        printf("3- Delete an activity.\n");
+        printf("4- Search an activity.\n");
+        printf("r/R- Return to menu.\n");
+        r=getch();
+    }
+    while (r!='r' && r!='R' && r!='1' && r!='2' && r!='3' && r!='4');
+    return r;
+}
+
+char formTeam()
+{
+    char r;
+    do
+    {
+        printf("---Team Section---\n");
+        printf("1- Insert a new team.\n");
+        printf("2- Show teams.\n");
+        printf("3- Delete a team.\n");
+        printf("4- Search a team.\n");
+        printf("r/R- Return to menu.\n");
+        r=getch();
+    }
+    while (r!='r' && r!='R' && r!='1' && r!='2' && r!='3' && r!='4');
+    return r;
+}
+
+char formTeamMan()
+{
+    char r;
+    do
+    {
+        printf("---Team Section---\n");
+        printf("1- Insert a new team.\n");
+        printf("2- Show teams.\n");
+        printf("3- Delete a team.\n");
+        printf("4- Search a team.\n");
+        printf("r/R- Return to menu.\n");
+        r=getch();
+    }
+    while (r!='r' && r!='R' && r!='1' && r!='2' && r!='3' && r!='4');
+    return r;
+}
+
 int main()
 {
     student_type student[MAX];
     int studentIndex;
+    char ch;
 
-    printf("Enter the number of students to save: ");
+    /*printf("Enter the number of students to save: ");
     scanf("%d", &studentIndex);
     insertStudent(student, studentIndex);
-    showStudents(student, studentIndex);
+    showStudents(student, studentIndex);*/
+    do
+    {
+        ch = form();
+        switch(ch)
+        {
+            case '1':
+                {//Manage students info
+                    do
+                    {
+                        ch = formStudents();
+                        switch(ch)
+                        {
+                            case '1':
+                                { //insert new students
+                                break;
+                                }
+                            case '2':
+                                { //show students
+                                break;
+                                }
+                            case '3':
+                                { //delete student
+                                break;
+                                }
+                            case '4':
+                                { //search student
+                                break;
+                                }
+                        }
+                    } while(ch!='r' && ch!='R');
+                    break;
+                }
+            case '2':
+                { //manage activity info
+                do
+                    {
+                        ch = formActivity();
+                        switch(ch)
+                        {
+                            case '1':
+                                { //insert new activity
+                                break;
+                                }
+                            case '2':
+                                { //show activities
+                                break;
+                                }
+                            case '3':
+                                { //delete activities
+                                break;
+                                }
+                            case '4':
+                                { //search activities
+                                break;
+                                }
+                        }
+                    } while(ch!='r' && ch!='R');
+                break;
+                }
+            case '3':
+                {// Team management
+                do
+                    {
+                        ch = formTeam();
+                        switch(ch)
+                        {
+                            case '1':
+                                { //insert new team
+                                break;
+                                }
+                            case '2':
+                                { //show teams (alphabetically sorted)
+                                break;
+                                }
+                            case '3':
+                                { //delete team
+                                break;
+                                }
+                            case '4':
+                                { //search teams and give option to show the elements
+                                break;
+                                }
+                        }
+                    } while(ch!='r' && ch!='R');
+                break;
+                }
+            case '4':
+                { //Teams info
+                do
+                    {
+                        ch = formTeamMan();
+                        switch(ch)
+                        {
+                            case '1':
+                                { //total and average right answers of a team
+                                break;
+                                }
+                            case '2':
+                                { //average age
+                                break;
+                                }
+                            case '3':
+                                { //less time wasted per activity
+                                break;
+                                }
+                        }
+                    } while(ch!='r' && ch!='R');
+                break;
+                }
+            case '5':
+                { //ordenar por idade
+                //ordenarPorIdade(aluno,qtd);
+                getch(); break;
+                }
+        }
+    } while(ch!='s' && ch!='S');
 
-    return 0;
+ return 0;
 }
